64d874bd5f5740c61fc5c8de3468c2e8ac399dba
lucius <luciholzinger@gmail.com>
2026-09-02 00:28:26 -0700

    implemented Chakra UI

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
+  // cacheComponents: true,
 };
 
 export default nextConfig;
