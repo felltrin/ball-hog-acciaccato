adc5fb6693917e5e52b7d13cb63aeb657864e645
Lucius Holzinger <luciholzinger@gmail.com>
2026-09-03 10:39:59 -0700

    All the content in the Navbar is added to remain consistent with the design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,18 @@
 
 import { Box, Container, Flex, Text } from "@chakra-ui/react";
 import { useColorModeValue } from "./ui/color-mode";
+import { IoMdPerson } from "react-icons/io";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
+// pink color RGB - (245, 200, 231)
 
 const Navbar = () => {
   return (
-    <Container maxW={"900px"}>
+    <Container maxW={"1200px"}>
       <Box
         px={4}
-        my={4}
-        borderRadius={5}
+        my={2}
+        borderRadius={50}
         bg={useColorModeValue("gray.200", "gray.700")}
         suppressHydrationWarning
       >
@@ -21,21 +25,35 @@ const Navbar = () => {
             gap={3}
             display={{ base: "none", sm: "flex" }}
           >
-            <img src="/react.png" alt="React logo" width={50} height={50} />
-            <Text fontSize={"40px"}>+</Text>
-            <img src="/python.png" alt="Python logo" width={50} height={40} />
-            <Text fontSize={"40px"}>=</Text>
+            <Box
+              px={4}
+              my={4}
+              borderLeftRadius={50}
+              bg={useColorModeValue("pink.200", "pink.200")}
+            >
+              <img src="/logo_dos.svg" alt="FM logo" width={80} height={80} />
+            </Box>
 
-            <img src="/explode.png" alt="Explode head" width={45} height={45} />
+            <Text fontSize={"16px"}>Insurance</Text>
+            <Text fontSize={"16px"}>Get a quote</Text>
+            <Text fontSize={"16px"}>Find an Agent</Text>
+            <Text fontSize={"16px"}>Claims</Text>
+            <Text fontSize={"16px"}>Resources</Text>
+            <Text fontSize={"16px"}>Payments</Text>
+
+            {/* <img src="/python.png" alt="Python logo" width={50} height={40} /> */}
+            {/* <img src="/explode.png" alt="Explode head" width={45} height={45} /> */}
           </Flex>
           {/* Right side */}
           <Flex gap={3} alignItems={"center"}>
+            <FaMagnifyingGlass />
+            <IoMdPerson />
             <Text
               fontSize={"lg"}
               fontWeight={500}
               display={{ base: "none", md: "block" }}
             >
-              BFFship 🔥
+              Log in
             </Text>
           </Flex>
         </Flex>
